1befdc7fed8616a1e0334815c435fd2233a45377
Lucas <lukinhassaito2013@hotmail.com>
2020-07-03 13:54:08 -0300

    create_table method added to database object and USERS table added

--- company.py
+++ company.py
@@ -1,15 +1,62 @@
 from database import database
+import datetime
 
+"""
+| NAME | SURNAME | EMAIL | CELLNUMB | CREATED |
+"""
+
+class Company(object):
+    def __init__(self):
+        pass
+    def add_employee(self):
+        pass
+    def remove_employee(self):
+        pass
+    def search_employee(self):
+        pass
+    def last_employees_created(self):
+        pass
 class Employee(object):
-    pass
+    def __init__(self):
+        pass
+        #self.name, self.surname = name, surname
+        #cself.email, self.cellnumb = email, cellnumb
+
+
+
 
 
 if __name__ == "__main__":
+    DATABASE_NAME = 'company_employees'
+
     config = {
         'host': 'localhost',
         'user': 'root',
         'pass': 'rootpass123',
-        'db': 'accounts'  # <-- Connects to database accounts
+        'db': DATABASE_NAME  # <-- Connects to database accounts
         }
     db = database(config['host'], config['user'],
                   config['pass'], config['db'])
+    """
+    Creating table "USERS":
+    table_name = "USERS"
+    columns = [('NAME', 'TEXT'), ('SURNAME', 'TEXT'),
+               ('EMAIL', 'TEXT'), ('CELLNUMB', 'TEXT'),
+               ('CREATED', 'DATETIME')]
+    db.create_table(table_name, columns)
+    
+    
+    ####### create a new table ########
+    db.create_table("teste", columns)     <-- (String, [Tuples])
+    
+    
+    
+    # DATETIME = '0000-00-00 00:00:00'  , YYYY-MM-DD
+    # DATETIME FORMAT
+    a = datetime.datetime.now()
+    x = a - datetime.timedelta(microseconds=a.microsecond)
+    print(x)"""
+
+
+
+
--- database.py
+++ database.py
@@ -1,4 +1,6 @@
 import mysql.connector as mysql
+from mysql.connector import errorcode
+
 #rootpass123
 
 
@@ -11,10 +13,28 @@ class database(object):
                 host= host, user= user,
                 passwd= user_pass, database= database_name
             )
-            print("Sucessfully connected to database: {}".format(database_name))
+            print("Successfully connected to database: {}.".format(database_name))
             self.cursor = self.connect.cursor()
+            self.tables = {}
         except mysql.Error as err:
             print(err)
+
+    def create_table(self, table_name, columns):  #<-- expects a string and a list of tuples
+        command = ['{} {}'.format(col[0], col[1]) for col in columns]
+        command = ', '.join(command)
+        try:
+            self.cursor.execute("CREATE TABLE {} ({});".format(table_name, command))
+            print("Table \"{}\" successfully created.".format(table_name))
+        except mysql.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                print("Table \"{}\" already exists.".format(table_name))
+            else:
+                print(err)
+
+    def remove_table(self):
+        pass
+    def show_tables(self):
+        pass
 
 
 
